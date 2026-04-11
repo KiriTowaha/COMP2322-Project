44e680787ba470e8eb945c8f3354269b29d0b7dd
KiriTowaha <64723622+KiriTowaha@users.noreply.github.com>
2026-04-12 03:45:07 +0800

    Ensure log directory exists before writing logs

--- src/log.py
+++ src/log.py
@@ -1,3 +1,4 @@
+import os
 import threading, datetime
 
 LOG_FILE = "./log/server.log"
@@ -5,6 +6,9 @@ LOG_FILE = "./log/server.log"
 log_lock = threading.Lock()
 
 def write_log(client_ip, status):
+    log_dir = os.path.dirname(LOG_FILE)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
     with log_lock:
         with open(LOG_FILE, "a") as log:
             time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
